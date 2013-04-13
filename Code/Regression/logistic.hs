1b9cf2427e9d180885708dd3dfe7a60ea0470d1c
rahulaaj <rahulajmera91@gmail.com>
2013-04-13 08:57:10 -0700

    logistic finished witout debug

--- Code/Regression/logistic.hs
+++ Code/Regression/logistic.hs
@@ -18,22 +18,33 @@ type Feature=[(String, Double)]
 --instance LogisticRegression l where
 rate=0.01
 weight=[]
+addToOld [] k1 val=[]
+addToOld ((k,v):xs) k1 val|(k1==k)=[(k,v+val)]++ xs
+                          | otherwise=[(k,v)]++ addToOld xs k1 val
+
+add dict k new=dict++[(k,new)]
+
 lookUp k []=False
 lookUp k ((k1,v):xs)|(k1==k) = True
                     | otherwise = lookUp k xs
+
 findVal k []=0
 findVal k ((k1,v):xs)|(k1==k) = v
-                    | otherwise = findVal k xs
+                     | otherwise = findVal k xs
+
 iterate weight []=
 iterate weight ((k,v):xs)|lookUp k weight = 0+(findVal k weight)*v+ iterate weight xs
                          | otherwise = 0+iterate weight xs
+
 classify feature= 1.0/(1.0+exp(-logit)) where
                 logit= iterate weight feature
+
 iterateTrain [] x weight = print "Done"
 iterateTrain ((k,v):xs) x weight|lookUp k weight= iterateTrain xs x (addToOld weight k new) where
                                                                 new=rate*(x-(classify ((k,v):xs)))*v
                                 | otherwise=iterateTrain xs x (add weight k (rate* update)) where
                                                         update= (x-(classify ((k,v):xs)))*v
+
 train 0 _=print "Complete"
 train n []=print $ show n
 train n ((x,feature):xs)= do iterateTrain feature x weight;
