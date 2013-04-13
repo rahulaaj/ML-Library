ab73ca542bd76e68816f211d8e0e750d2199ba09
divyanshu <divbhar@iitk.ac.in>
2013-04-13 16:17:25 +0530

    KMeans

--- K-Means.hs
+++ K-Means.hs
@@ -5,30 +5,37 @@ import Data.Ord
 number :: [[Double]]->Int
 number points= length points
 
+--find the sum component wise 
 sumVectors :: [[Double]]->[Double]
 sumVectors points = map sum $ transpose points
   
+--find the centroid of a collection of points
 centroid :: [[Double]]->[Double]
 centroid points = map f $ sumVectors points 
   where
     f a = a / ( fromIntegral $ number points)
     
+--find distance between 2 points
 dist :: [Double]->[Double]-> Double
 dist x y = sqrt $ sum $ zipWith f x y
   where
     f a b=(a-b)*(a-b)
     
+--find the minimum distance between a ref point and the collection points
 minDist :: [Double]->[[Double]]->[Double]
 minDist refPoint points = minimumBy (comparing $ dist refPoint) points
 
+--find the centroid of each point
 findCentroid :: [[Double]]->[[Double]]->[([Double],[Double])]
 findCentroid centroids points = map (\x -> (minDist x centroids,x)) points
 
+--make cluster given centroids and points
 makeCluster :: [[Double]]->[[Double]]->[[[Double]]]
 makeCluster centroids points =map (map snd) $ groupBy f (findCentroid centroids points)
   where
     f (a,b) (c,d) = a==c
     
+--divide the list of points into clusters of k size
 dividePoints :: Int->[[Double]]->[[[Double]]]
 dividePoints k points 
   | second==[] = [first]
@@ -37,6 +44,7 @@ dividePoints k points
     (first,second)=splitAt l points
     l = (length points + k - 1) `div` k
 
+--initates cluster formation
 clustering :: [[[Double]]]->[[[Double]]]
 clustering clusters 
   | newClusters==clusters = clusters
@@ -46,5 +54,6 @@ clustering clusters
     centroids = map centroid clusters
     points = concat clusters
   
+--returns the clusters
 kmeans :: Int->[[Double]]->[[[Double]]]
 kmeans k points = clustering $ dividePoints k points
