70a5ecc4f30900dafb090de760cba18c446b7b61
divyanshu <divbhar@iitk.ac.in>
2013-04-17 03:40:20 +0530

    test

--- kmeans_test.hs
+++ kmeans_test.hs
@@ -1,7 +1,6 @@
---import KMeans
+import KMeans
 import System.IO
 
 main=do
-	pts<-readFile "test_kmeans.txt"
-	let points= map (map read.words) $ lines pts
-	print points
+	--print $ kmeans 2 [[1.0,1.0],[1.5,2.0],[3.0,4.0],[5.0,7.0],[3.5,5.0],[4.5,5.0],[3.5,4.5]]
+	print $ kmeans 3 [[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0],[1.0,3.0,5.0],[2.0,4.0,6.0],[3.0,5.0,7.0],[4.0,6.0,8.0],[5.0,7.0,9.0],[6.0,8.0,0.0]]
