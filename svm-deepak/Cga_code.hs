40bab937649716b9c6cfe0dcbd6d8b39a2e7ada0
Deepak Pathak <deepak.pathak2206@gmail.com>
2013-04-17 00:36:18 -0700

    svm

--- svm-deepak/Cga_code.hs
+++ svm-deepak/Cga_code.hs
@@ -1,11 +1,14 @@
 --This file contains implementation of Conjugate Gradient Algorithm which is used to find optimal solution of Langrangian. It finds
 --the saddle point of the dual of the Lagrangian.
 
+{-# OPTIONS_GHC -XBangPatterns #-}			-- For bang in function arguements to avoid laziness
+
 module Cga_code where
 
 	import Data.Array.Unboxed
 	import Data.List (foldl')
 	import Numeric.LinearAlgebra
+	import Debug.Trace
 	-- algebra for matrices
 	-- Unboxed arrays are used for eager evaluation of elements of array. (performance improve)
 	-- foldl' gives better performance than sum due to eager evaluation and tailoptimisation.
@@ -18,19 +21,20 @@ module Cga_code where
 	-- Conjugate Gradient Algorithm: It will run until a cutoff delta or max_iter is reached.
 	-- x:initially assumed solution , p:initial p_0 , r:initial r_0 , km: matrix A , epsilon:cutoff delta , max_iter:maximum number of iterations
 	-- Acknowledgement: Refer wikipedia for iterative version of conjugate algorithm.
-	conjugateGradientAlgo :: KernelMatrix -> Array_doubles -> Array_doubles -> Array_doubles -> Double -> Int -> Array_doubles
-	conjugateGradientAlgo (KernelMatrix km) x p r epsilon max_iter = cga_aux delta_initial max_iter x p r False
+	-- conjugateGradientAlgo :: KernelMatrix -> Array_doubles -> Array_doubles -> Array_doubles -> Double -> Int -> Array_doubles
+	conjugateGradientAlgo !(KernelMatrix km) x p r epsilon max_iter !a = cga_aux delta_initial max_iter x p r False
 		 where 	
 				delta_initial = aDot r r
 				cga_aux _ _ x _ _ True = x			--break loop
 				cga_aux delta iter x p r _ =
 					let	km_p = km <> (buildMatrix (rows km) 1 (\(i,j) -> (p!(i+1))))		-- n*1 matrix
-						oneByOne = ((buildMatrix 1 (rows km) (\(i,j) -> (p!(i+1)))) <> km_p)
+						oneByOne = ((buildMatrix 1 (rows km) (\(i,j) -> (p!(j+1)))) <> km_p)
 						alpha1 = delta / (det oneByOne)
-						x_new = aAdd x $ scalar_mult alpha1 (elems p)
-						r_new = aAdd r $ scalar_mult (-alpha1) (toList $ flatten km_p)
+						a = trace ("here5 x2="++(show (x!2))++" p2="++(show (p!2)++" alpha1="++(show alpha1)++" onebyone="++(show oneByOne)++" det="++(show (det oneByOne))++" p="++(show p)++" pmat="++(show (buildMatrix 1 (rows km) (\(i,j) -> (p!(j+1))))))) True
+						x_new = aAdd x $ scalar_mult a alpha1 p
+						r_new = aAdd r $ scalar_mult a (-alpha1) (listArray (1,rows km) (toList $ flatten km_p))
 						delta_new = aDot r_new r_new
-						p_new = aAdd r_new $ scalar_mult (delta_new/delta) (elems p)
+						p_new = aAdd r_new $ scalar_mult a (delta_new/delta) p
 						done = (delta_new < epsilon*delta_initial) || (iter == 0)
 					in  
 						cga_aux delta_new (iter-1) x_new p_new r_new done
@@ -39,12 +43,8 @@ module Cga_code where
 	--Following helper functions over arrays have been used :
 
 	--Multiply each element with a scalar
-	scalar_mult :: Double -> [Double] -> Array_doubles
-	scalar_mult a l = 
-			let	temp = map (*a) l
-				len = length temp
-			in
-				(listArray (1,len) temp)
+	-- scalar_mult :: Double -> Array_doubles -> Array_doubles
+	scalar_mult !a = amap . (*)
 
 	--ZipWith for arrays
 	aZipWith :: (Double -> Double -> Double) -> Array_doubles -> Array_doubles -> Array_doubles
